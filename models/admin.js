ae83cc9083ca8ddaf67393883101e4b1bd1fb346
thecoder-droid <thegabrielfrancis@gmail.com>
2020-09-23 10:53:09 -0400

    first commit new repo

---
+++ models/admin.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const Admin = sequelize.define('admin', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true
+        },
+        tempPassword: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN
+        }
+    })
+    return Admin;
+}
