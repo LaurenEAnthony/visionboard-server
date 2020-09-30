fe6422647caff20d0fcc7c187739441843e865c0
Lauren Anthony <laurenanthony247@gmail.com>
2020-09-29 21:02:24 -0400

    deleted admin model/controller, added associations to all models, updated usercontroller all endpoints, updated boardcontroller minor changes to endpoints & moved get items by board endpoint from itemcontroller to boardcontroller

--- controllers/boardcontroller.js
+++ controllers/boardcontroller.js
@@ -1,18 +1,18 @@
-let express = require('express');
-const board = require('../models/board');
+let express = require("express");
 let router = express.Router();
 // let sequilize = require('../db');
 // const Board = sequilize.import('../models/board.js');
-const Board = require('../db').import('../models/board');
+const Board = require("../db").import("../models/board");
 
 // let validateSession = require('../middleware/validate-session');
 
-
-router.post('/practice',
-// valisateSession,
-function(req,res) {
-    res.send('Hey Board');
-})
+router.post(
+  "/practice",
+  // valisateSession,
+  function (req, res) {
+    res.send("Hey Board");
+  }
+);
 
 /*
 Board: ~/api/board
@@ -23,69 +23,79 @@ GET/mine           => Find all my boards
 GET/shared         => Get boards that other users have shared
 */
 
-
 //Create Board
-router.post('/create', function (req, res) {
-    const boardEntry = {
-        // owner: req.user.id,
-        boardTitle: req.body.board.boardTitle,
-        description: req.body.board.description,
-        dateCreated: req.body.board.dateCreated,
-        tags: req.body.board.tags,
-        sharedBoard: req.body.board.sharedBoard
-    }
-    Board.create(boardEntry)
-    .then(board => res.status(200).json(board))
-    .catch(err => res.status(500).json({error: err}))
+router.post("/create", function (req, res) {
+  const boardEntry = {
+    // owner: req.user.id,
+    boardTitle: req.body.board.boardTitle,
+    description: req.body.board.description,
+    dateCreated: req.body.board.dateCreated,
+    tags: req.body.board.tags,
+    sharedBoard: req.body.board.sharedBoard,
+  };
+  Board.create(boardEntry)
+    .then((board) => res.status(200).json(board))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 // //Update board
-router.put('/update/:entryId', function (req, res) {
-    const updateBoardEntry = {
-        boardTitle: req.body.board.boardTitle,
-        description: req.body.board.description,
-        dateCreated: req.body.board.dateCreated,
-        tags: req.body.board.tags,
-        sharedBoard: req.body.board.sharedBoard
-    };
+router.put("/update/:entryId", function (req, res) {
+  const updateBoardEntry = {
+    boardTitle: req.body.board.boardTitle,
+    description: req.body.board.description,
+    dateCreated: req.body.board.dateCreated,
+    tags: req.body.board.tags,
+    sharedBoard: req.body.board.sharedBoard,
+  };
 
-    const query = { where: {id: req.params.entryId, owner: req.user.id}};
+  const query = { where: { id: req.params.entryId, owner: req.user.id } };
 
-    Board.update(updateBoardEntry, query)
+  Board.update(updateBoardEntry, query)
     .then((boards) => res.status(200).json(boards))
-    .catch((err) => res.status(500).json({error:  err}));
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 //Delete Board
-router.delete('/delete/:id', function (req, res) {
-    const query = { where: { id: req.params.id, owner: req.user.id }};
+router.delete("/delete/:id", function (req, res) {
+  const query = { where: { id: req.params.id, owner: req.user.id } };
 
-   Board.destroy(query)
-    .then(() => res.status(200).json({ message: "This board has been removed"  }))
-    .catch((err) => res.status(500).json({ error: err } ));
+  Board.destroy(query)
+    .then(() =>
+      res.status(200).json({ message: "This board has been removed" })
+    )
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
 //Get all boards for individual user
-router.get('/mine', (req, res) => {
-    let userid = req.user.id
-    Board.findAll({
-        where: {owner: userid}
-    })
-    .then(board => res.status(200).json(board))
-    .catch(err => res.status(500).json({error: err}))
+router.get("/mine", (req, res) => {
+  let userid = req.user.id;
+  Board.findAll({
+    where: { owner: userid },
+  })
+    .then((board) => res.status(200).json(board))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
-//Get shared boards 
-router.get('/shared', (req, res) => {
-    // let shared = req.params.shared;
-
-    Board.findAll({
-        where: {sharedBoard: true, owner: req.user.id}
-    })
-    .then(board => res.status(200).json(board))
-    .catch(err => res.status(500).json({error: err}))
-    //res.send("It worked")
+//Get all items for a particular board   ????????
+router.get("/:boardId", (req, res) => {
+  let boardid = req.board.id;
+  Item.findAll({
+    where: { board: boardid },
+  })
+    .then((item) => res.status(200).json(item))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
+//Get shared boards
+router.get("/shared", (req, res) => {
+  // let shared = req.params.shared;
+
+  Board.findAll({
+    where: { sharedBoard: true, owner: req.user.id },
+  })
+    .then((board) => res.status(200).json(board))
+    .catch((err) => res.status(500).json({ error: err }));
+  //res.send("It worked")
+});
 
 module.exports = router;
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,58 +1,139 @@
-const router = require('express').Router();
-const User = require('../db').import('../models/user');
+const router = require("express").Router();
+const User = require("../db").import("../models/user");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const validateSession = require("../middleware/validate-session");
 
-router.post('/signup', function(req, res) {
-
-    User.create({
-        firstName: req.body.user.firstName,
-        lastName: req.body.user.lastName, 
-        email: req.body.user.email,
-        password: bcrypt.hashSync(req.body.user.password, 13),
-        colorScheme: req.body.user.colorScheme
-    })
-    .then(
-        function createSuccess(user) {
-            let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
-
-            res.json({
-                user: user,
-                message: 'User successfully created!',
-                sessionToken: token
-            });
-        }
-    )
-    .catch(err => res.status(500).json({ error: err }))
-});
-
-router.post('/login', function(req, res) {
-    User.findOne({
-        where: {
-            email:req.body.user.email   
-        }
-    })
+// ***LOGIN***
+router.post("/login", function (req, res) {
+  User.findOne({
+    where: {
+      email: req.body.user.email,
+    },
+  })
     .then(function loginSuccess(user) {
-        if (user) {
-            bcrypt.compare(req.body.user.password, user.password, function (err, matches) {
-                if (matches) {
-                    let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
-
-                    res.status(200).json({
-                        user: user,
-                        message: 'User successfully logged in!',
-                        sessionToken: token
-                    })
-                } else {
-                    res.status(502).send({ error: "Login Failed" });
-                }
+      if (user) {
+        bcrypt.compare(req.body.user.password, user.password, function (
+          err,
+          matches
+        ) {
+          if (matches) {
+            let token = jwt.sign(
+              { id: user.id, isAdmin: user.isAdmin },
+              process.env.JWT_SECRET,
+              {
+                expiresIn: 60 * 60 * 24,
+              }
+            );
+            res.status(200).json({
+              user: user,
+              message: "User successfully logged in!",
+              sessionToken: token,
             });
-
-        } else {
-            res.status(500).json({ error: 'User does not exist.' })
-        }
+          } else {
+            res.status(502).send({ error: "Login Failed" });
+          }
+        });
+      } else {
+        res.status(500).json({ error: "User does not exist." });
+      }
     })
-    .catch(err => res.status(500).json({ error: err }))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
-module.exports = router;   
+// ***REGULAR USER SIGNUP***
+router.post("/signup", function (req, res) {
+  User.create({
+    firstName: req.body.user.firstName,
+    lastName: req.body.user.lastName,
+    email: req.body.user.email,
+    password: bcrypt.hashSync(req.body.user.password, 13),
+    colorScheme: 0,
+    isAdmin: false,
+  })
+    .then(function createSuccess(user) {
+      let token = jwt.sign(
+        { id: user.id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 60 * 60 * 24,
+        }
+      );
+
+      res.json({
+        user: user,
+        message: "User successfully created!",
+        sessionToken: token,
+      });
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+});
+
+// ***ADMIN USER SIGNUP***
+router.post("/admin/signup", validateSession, function (req, res) {
+  const admin = req.user.isAdmin;
+  if (admin == true) {
+    User.create({
+      firstName: req.body.user.firstName,
+      lastName: req.body.user.lastName,
+      email: req.body.user.email,
+      password: bcrypt.hashSync(req.body.user.password, 13),
+      colorScheme: 0,
+      isAdmin: true,
+    })
+      .then(function createSuccess(user) {
+        let token = jwt.sign(
+          { id: user.id, isAdmin: user.isAdmin },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: 60 * 60 * 24,
+          }
+        );
+
+        res.json({
+          user: user,
+          message: "Admin successfully created!",
+          sessionToken: token,
+        });
+      })
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.status(500).json({ error: "Not Authorized" });
+  }
+});
+
+// ***ADMIN VIEW USER DATA***
+router.get("/admin/view-all", validateSession, function (req, res) {
+  const admin = req.user.isAdmin;
+  if (admin == true) {
+    User.findAll()
+      .then((user) => res.status(200).json(user))
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.status(502).json({ error: "Not Authorized" });
+  }
+});
+
+// ***ADMIN EDIT USER DATA***
+router.put("/admin/:userId", validateSession, function (req, res) {
+  const admin = req.user.isAdmin;
+  if (admin == true) {
+    const updateUser = {
+      firstName: req.body.user.firstName,
+      lastName: req.body.user.lastName,
+      email: req.body.user.email,
+      password: bcrypt.hashSync(req.body.user.password, 13),
+      isAdmin: req.body.user.isAdmin,
+    };
+
+    const query = { where: { id: req.params.userId } };
+
+    User.update(updateUser, query)
+      .then((user) => res.status(200).json(user))
+      .catch((err) => res.status(500).json({ error: err }));
+  } else {
+    res.status(502).json({ error: "Not Authorized" });
+  }
+});
+
+module.exports = router;
